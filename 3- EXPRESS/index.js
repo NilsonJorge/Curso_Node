19c8021c78e30469f88ed67ebb533161ab5af3a0
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-05-24 11:10:13 -0300

    first commit

--- 3- EXPRESS/index.js
+++ 3- EXPRESS/index.js
@@ -7,12 +7,32 @@ app.get("/", function(req, res){
     );     
 });
 
-app.get("/blog", function(req, res){
-    res.send("Bem vindo ao meu blog!")
+app.get("/blog/:artigo?", function(req, res){
+
+    var artigo = req.params.artigo;
+    if(artigo){
+        res.send(`<h2>Artigo: ${artigo}</h2>`)
+    }else{
+        res.send(`<h2>Bem vindo ao meu blog!</h2>`)
+    }
+    
 })
 
 app.get("/canal/youtube", function(req, res){
-    res.send("Bem vindo ao meu canal!")
+
+    var canal = req.query["canal"];
+    if(canal){
+        res.send("Canal: "+canal)
+    }else{
+        res.send("Não foi informado o canal")
+    }
+    
+})
+
+app.get('/ola/:nome/:empresa', function(req,res){
+    const nome = req.params.nome
+    const empresa = req.params.empresa
+    res.send(`<h1>Oi ${nome} que trabalha na empresa ${empresa}!</h1>`)
 })
 
 app.listen(4000,function(erro){
--- anotacoes.markdown
+++ anotacoes.markdown
@@ -1,3 +1,7 @@
 ## Instalar Express
 
     npm install express --save
+
+## Instalar ferramenta para atualização automatica do server
+
+    npm install nodemon -g

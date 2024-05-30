9d2a173b2e95ddbf59626fad4f04c705aeb009b2
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-05-30 17:28:35 -0300

    second project

--- 7- Guiapress/index.js
+++ 7- Guiapress/index.js
@@ -3,6 +3,12 @@ const app = express();
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
 
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+
+const Article = require('./articles/Article')
+const Category = require('./categories/Category')
+
 //View engine
 app.set('view engine', 'ejs');
 
@@ -21,6 +27,10 @@ connection.authenticate()
     .catch((error) => {
         console.log(error)
     })
+
+app.use("/", categoriesController);
+
+app.use('/', articlesController);
 
 app.get("/", (req,res) => {
     res.render('index')

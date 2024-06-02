0d31a491f7cf828b2aace8a02d61cbc4e64a4b32
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-06-02 11:55:13 -0300

    second project

--- 7- Guiapress/index.js
+++ 7- Guiapress/index.js
@@ -7,7 +7,8 @@ const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
 
 const Article = require('./articles/Article')
-const Category = require('./categories/Category')
+const Category = require('./categories/Category');
+const { where } = require('sequelize');
 
 //View engine
 app.set('view engine', 'ejs');
@@ -34,9 +35,12 @@ app.use('/', articlesController);
 
 app.get("/", (req,res) => {
     Article.findAll({
-        order: [['id', "DESC"]]
+        order: [['id', "DESC"]],
+        limit: 4
     }).then(articles => {
-        res.render('index',{articles: articles})
+        Category.findAll().then(categories => {
+            res.render('index',{articles: articles, categories: categories})
+        })
     })
     
 })
@@ -49,13 +53,36 @@ app.get("/:slug", (req,res) => {
         }
     }).then(article => {
         if(article){
-            res.render("article", {article: article});
+            Category.findAll().then(categories => {
+                res.render('article',{article: article, categories: categories})
+            })
         }else{
             res.redirect('/')
         }
     }).catch(err => {
         res.redirect('/');
     })
+})
+
+app.get('/category/:slug', (req,res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        }, include:[{model: Article}]
+    }).then(category => {
+        if(category != undefined){
+            
+            Category.findAll().then(categories => {
+                res.render('index', {articles: category.articles, categories: categories})
+            })
+
+        }else{
+            res.redirect('/');
+        }
+    }).catch(err => {
+        res.redirect('/');
+    }) 
 })
 
 app.listen(8080, () => {
